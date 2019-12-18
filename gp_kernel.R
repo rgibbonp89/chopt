02d568177eedf253a827af72d85934d7a9a94f12
Ubuntu <ubuntu@ip-172-31-16-174.eu-west-1.compute.internal>
2019-12-18 16:53:06 +0000

    dmvnorm and output from .c dont correspond - needs to be investigated

--- gp_kernel.R
+++ gp_kernel.R
@@ -1,25 +1,20 @@
-dyn.load("kernel.so")
-nr = 5e2
-nc = 5e1
-vec = rep(0, nr)
-mat = matrix(c(rnorm(nr*nc, -2, 1)), nrow = nr)
-
-f <- function(r, c, v, m){
-    .C("run", row=as.integer(r), column=as.integer(c), vec=v, mat=m)
-    }
-
-out = f(nr, nc, vec, mat)
-
 # for matrix stuff
+
+library('MASS')
+library('emdbook')
 
 dyn.load("kernel.so")
 nr = 1e2
 nc = 5
 vec = rep(0, nr)
-mat = matrix(c(rnorm(nr*nc, 0, 1)), nrow = nr)
+samples = mvrnorm(nr, c(rep(0, nc)), diag(nc))
+mat = samples
 
 f <- function(r, c, v, m){
     .C("kernel", row=as.integer(r), column=as.integer(c), mat=m, vec=v)
     }
 
 out = f(nr, nc, vec, mat)
+
+dmvnorm(samples, c(rep(0, nc)), diag(nc))
+
