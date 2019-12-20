60f3ae16445c78363ea5af496b271f68528b1072
Ubuntu <ubuntu@ip-172-31-16-174.eu-west-1.compute.internal>
2019-12-20 14:48:22 +0000

    RBF successfully calculated - corresponds exactly to reference

--- gp_kernel.R
+++ gp_kernel.R
@@ -4,14 +4,14 @@ library('MASS')
 library('pracma')
 
 dyn.load("kernel.so")
-nrx = 2e4
-nry = 3e3
-nc = 3e2
+nrx = 20
+nry = 10 
+nc = 5
 mat_x = mvrnorm(nrx, c(rep(0, nc)), diag(nc))
 mat_y = mvrnorm(nry, c(rep(0, nc)), diag(nc))
 
 
-f <- function(mx, my){
+f <- function(mx, my, param){
     xr = nrow(mx)
     yr = nrow(my)
     c = ncol(mx)
@@ -22,15 +22,16 @@ f <- function(mx, my){
     my <- c(t(my))
     }
     rv = rep(0, xr*yr) 
-    out = .C("rbf_kernel", xrow=as.integer(xr), yrow=as.integer(yr), 
-    column=as.integer(c), mat_x=mx, mat_y=my, mat_res=rv)
-    output = out$mat_res
-    output_mat = matrix(output, nrow = xr, ncol = yr, byrow = T)
-    return(output_mat)
+    out = .C("rbf_kernel", param=param, xrow=as.integer(xr), yrow=as.integer(yr), 
+    column=as.integer(c), mat_x=mx, mat_y=my, mat_res=rv, mat_prod=rv, mat_kernel=rv)
+    out$mat_prod = matrix(out$mat_prod, nrow = xr, ncol = yr, byrow = T)
+    out$mat_res = matrix(out$mat_res, nrow = xr, ncol = yr, byrow = T)
+    out$mat_kernel = matrix(out$mat_kernel, nrow = xr, ncol = yr, byrow = T)
+    return(out)
     }
 
 start_time <- Sys.time()
-out = f(mat_x, mat_y)
+out = f(mat_x, mat_y, 1.0)
 end_time <- Sys.time()
 end_time - start_time
 
@@ -41,3 +42,5 @@ end_time - start_time
 
 
 
+y = matrix(c(-0.23, -.21, 1.2, -0.23, 1.87, 0.3), nrow =2,  byrow=T)
+x = matrix(c(0.01, 0.05, 0.12, 0.32, 1.20, -2.1), nrow = 2, byrow=T)
--- kernel.c
+++ kernel.c
@@ -4,6 +4,7 @@
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_blas.h>
+#include <math.h>
 
 /*
 @xrow = count of the number of rows of the input matrix x
@@ -14,8 +15,8 @@
 @results_matrix = vector of zeros that the function will write to and that will be output
 */
 
-int rbf_kernel(int *xrow, int *yrow, int *col, double *input_matrix_x,
-double *input_matrix_y, double *results_matrix)
+int rbf_kernel(double *param_in, int *xrow, int *yrow, int *col, double *input_matrix_x,
+double *input_matrix_y, double *results_matrix, double *product_matrix, double *kernel_matrix)
 {
     // Number of elements of mu is length(nc) and vcov is nc*nc
     size_t nrx = xrow[0];
@@ -25,29 +26,63 @@ double *input_matrix_y, double *results_matrix)
     int counterx = 0;
     int countery = 0;
     int counterout = 0;
+    double param = param_in[0];
 
     // Convert passed r matrix to gsl matrix
-    // Loops to assign values to the allocated matrix
     gsl_matrix * x = gsl_matrix_alloc(nrx, nc);
     gsl_matrix * y = gsl_matrix_alloc(nry, nc);
+    gsl_matrix * xsq = gsl_matrix_alloc(nrx, 1);
+    gsl_matrix * ysq = gsl_matrix_alloc(nry, 1);
+    gsl_matrix * xycombine = gsl_matrix_alloc(nrx, nry);
     gsl_matrix * output_matrix = gsl_matrix_alloc(nrx, nry);
 
+    // Need to assign values to matrices and calcuilate euclidean norm for each row
+    // This happens for x and y matrix
     for (i=0;i<nrx;i++)
         for (j=0;j<nc;j++)
-        {
+            {
              gsl_matrix_set(x, i, j, input_matrix_x[counterx++]);
+            }
+
+    // For each row in turn, we extract it and then calculate \sum x_i{i}^{2}
+    for (i=0;i<nrx;i++)
+        {
+        gsl_vector * tmp_x_vec = gsl_vector_alloc(nc);
+        gsl_matrix_get_row(tmp_x_vec, x, i);
+        double euclideanx = gsl_blas_dnrm2(tmp_x_vec)*gsl_blas_dnrm2(tmp_x_vec);
+        gsl_matrix_set(xsq, i, 0, euclideanx);
         }
 
     for (i=0;i<nry;i++)
         for (j=0;j<nc;j++)
-        {
+            {
              gsl_matrix_set(y, i, j, input_matrix_y[countery++]);
+            }
+
+    for (i=0;i<nry;i++)
+        {
+        gsl_vector * tmp_y_vec = gsl_vector_alloc(nc);
+        gsl_matrix_get_row(tmp_y_vec, y, i);
+        double euclideany = gsl_blas_dnrm2(tmp_y_vec)*gsl_blas_dnrm2(tmp_y_vec);
+        gsl_matrix_set(ysq, i, 0, euclideany);
         }
+
+    // Then calculate xsq + Tysq
+    for (i=0;i<nrx;i++)
+        for (j=0;j<nry;j++)
+            {
+            double tmp_entry_x = gsl_matrix_get(xsq, i, 0);
+            double tmp_entry_y = gsl_matrix_get(ysq, j, 0);
+            gsl_matrix_set(xycombine, i, j, tmp_entry_x+tmp_entry_y);
+            }
 
     // Perform matrix multiplication and assign to output_matrix
     gsl_blas_dgemm (CblasNoTrans, CblasTrans,
                   1.0, x, y,
                   0.0, output_matrix);
+
+    double scaler = 2.0;
+    gsl_matrix_scale(output_matrix, scaler);
 
     // Finally, assign to results_matrix for use in R
     for(i=0;i<nrx;i++)
@@ -55,4 +90,23 @@ double *input_matrix_y, double *results_matrix)
         {
         results_matrix[counterout++] = gsl_matrix_get(output_matrix, i, j);
         }
+
+    int counteroutm = 0;
+    for(i=0;i<nrx;i++)
+        for(j=0;j<nry;j++)
+        {
+        product_matrix[counteroutm++] = gsl_matrix_get(xycombine, i, j);
+        }
+
+    gsl_matrix_sub(xycombine, output_matrix);
+    double cont = -.5*(1.0/param);
+    gsl_matrix_scale(xycombine, cont);
+
+    int counteroutcombined = 0;
+    for(i=0;i<nrx;i++)
+        for(j=0;j<nry;j++)
+        {
+        kernel_matrix[counteroutcombined++] = exp(gsl_matrix_get(xycombine, i, j));
+        }
+
 }
